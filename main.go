38291c457361e47c9037d8cad2dbd15c1b9d07db
saqlain <saqlainsibtain123@gmail.com>
2024-09-24 14:27:42 +0530

    added model and basic rendering for the tui

--- main.go
+++ main.go
@@ -1,1 +1,86 @@
 package main
+
+import (
+	"fmt"
+	"os"
+
+	"github.com/charmbracelet/bubbles/list"
+	tea "github.com/charmbracelet/bubbletea"
+)
+
+type status int
+
+const (
+	todo status = iota
+	inProgress
+	done
+)
+
+//encapsulates data related to the task
+type Task struct {
+	status      status
+	title       string
+	description string
+}
+
+// implementing the list.Item interface
+func (t Task) FilterValue() string {
+	return t.title
+}
+
+func (t Task) Title() string {
+	return t.title
+}
+
+func (t Task) Description() string {
+	return t.description
+}
+
+//  MAIN MODEL
+
+type Model struct {
+	list list.Model
+	err  error
+}
+
+func New() *Model {
+	return &Model{}
+}
+
+func (m *Model) initList(width, height int) {
+	m.list = list.New([]list.Item{}, list.NewDefaultDelegate(), width, height)
+	m.list.Title = "TO DO"
+	m.list.SetItems([]list.Item{
+		Task{status: todo, title: "buy milk", description: "stawberry milk"},
+		Task{status: todo, title: "eat sushi", description: "idk what roll, some soup"},
+		Task{status: todo, title: "fold laundry", description: "wear clothes"},
+	})
+}
+
+func (m Model) Init() tea.Cmd {
+	return nil
+}
+
+func (m Model) Update(msg tea.Msg) (tea.Model, tea.Cmd) {
+	switch msg := msg.(type) {
+	case tea.WindowSizeMsg:
+		m.initList(msg.Width, msg.Height)
+	}
+	var cmd tea.Cmd
+	m.list, cmd = m.list.Update(msg)
+	return m, cmd
+}
+
+func (m Model) View() string {
+	return m.list.View()
+}
+
+func main()  {
+	m := New()
+	p := tea.NewProgram(m)
+
+	if err := p.Start(); err != nil {
+		fmt.Println(err)
+		os.Exit(1)
+	}
+}
